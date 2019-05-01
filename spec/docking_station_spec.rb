8ad2e8fd7530f5c357fa76f59148a5443948abe5
Max Van Aken <mtvanaken@gmail.com>
2019-05-01 15:42:58 +0100

    Created unit test to respond to release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
 
-describe DockingStation do
+# describe DockingStation do
+#   it "responds to release bike" do
+#     docking_station = DockingStation.new
+#     expect(docking_station).to respond_to(:release_bike)
+#   end
+# end
 
+describe DockingStation do
+  docking_station = DockingStation.new
+  it { is_expected.to respond_to(:release_bike) }
 end
