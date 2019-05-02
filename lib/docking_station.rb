cda9a0d557fe98c581d14c80db2b1d7062b895e2
Max Van Aken <mtvanaken@gmail.com>
2019-05-02 16:07:06 +0100

    allow docking station to recieve bike, allow user to see bikes available, and only allow bikes to be released if there are bikes available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,24 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike_array
+  def initialize
+    @bike_array = []
+  end
 
   def release_bike
-      bike = Bike.new
-      return bike
+    if @bike_array.length > 0
+      Bike.new
+    else
+      raise StandardError
+    end
   end
 
-  def receive_bike
+  def receive_bike(bike)
+    @bike_array << bike
   end
 
+  def check_bikes
+    @bike_array
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,9 +10,28 @@ require 'docking_station'
 describe DockingStation do
   docking_station = DockingStation.new
   it { is_expected.to respond_to(:release_bike) }
-  it "provides a bike that  responds to working?" do
-      bike = subject.release_bike
+  it "provides a bike" do
+    docking_station_with_bike = DockingStation.new
+    docking_station_with_bike.receive_bike(Bike.new)
+    expect(docking_station_with_bike.release_bike).to be_instance_of Bike
+  end
+  it "provides a bike that responds to working?" do
+      docking_station_with_bike = DockingStation.new
+      docking_station_with_bike.receive_bike(Bike.new)
+      bike = docking_station_with_bike.release_bike
       expect(bike).to be_working
   end
   it { is_expected.to respond_to(:receive_bike) }
+  it "checks if the bike is being stored in docking station" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.receive_bike(bike)
+    expect(station.bike_array.include? bike).to eq(true)
+  end
+  it "checks what bikes are in bike_array" do
+    expect(subject.check_bikes).to eq(subject.bike_array)
+  end
+  it "releasing a bike when bike_array is empty raises an error" do
+    expect { subject.release_bike }.to raise_error if subject.bike_array.length == 0
+  end
 end
